c0f897596cc91c2552df3bd618374f3b24e536fe
maybemaby <74226064+maybemaby@users.noreply.github.com>
2020-12-04 21:45:21 -0800

    Fixed accidental price grabs from post titles.

--- buildapcsales-cheapestparts.py
+++ buildapcsales-cheapestparts.py
@@ -5,19 +5,20 @@ import requests, bs4,logging, re
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
 logging.disable(logging.CRITICAL)
 
-PartCategory = {'CPU':10000,'GPU':10000, 'RAM':10000, 'HDD':10000, 'SSD':10000, 'Motherboard':10000, 'Monitor':10000,
-'PSU':10000, 'Webcam':10000, 'Controller':10000, 'Laptop':10000, 'Other':10000, 'VR':10000, 'Case':10000, 'Cooler':10000,
-'Headphones':10000, 'Fan':10000, 'Prebuilt':10000, 'Headset':10000, 'Optical Drive':10000, 'OS':10000, 'Speakers':10000,
-'Keyboard':10000, 'Networking':10000, 'Furniture':10000, 'Mouse':10000, 'Bundle':10000, 'HTPC':10000, 'Cables':10000,
-'Flash Drive':10000, 'Router':10000, 'Mic':10000}
+PartCategory = {'[CPU]':10000,'[GPU]':10000, '[RAM]':10000, '[HDD]':10000, '[SSD]':10000, '[Motherboard]':10000, '[Monitor]':10000,
+'[PSU]':10000, '[Webcam]':10000, '[Controller]':10000, '[Laptop]':10000, '[Other]':10000, '[VR]':10000, '[Case]':10000, '[Cooler]':10000,
+'[Headphones]':10000, '[Fan]':10000, '[Prebuilt]':10000, '[Headset]':10000, '[Optical Drive]':10000, '[OS]':10000, '[Speakers]':10000,
+'[Keyboard]':10000, '[Networking]':10000, '[Furniture]':10000, '[Mouse]':10000, '[Bundle]':10000, '[HTPC]':10000, '[Cables]':10000,
+'[Flash Drive]':10000, '[Router]':10000, '[Mic]':10000}
+PartLinks = {'[CPU]':'','[GPU]':'', '[RAM]':'', '[HDD]':'', '[SSD]':'', '[Motherboard]':'', '[Monitor]':'',
+'[PSU]':'', '[Webcam]':'', '[Controller]':'', '[Laptop]':'', '[Other]':'', '[VR]':'', '[Case]':'', '[Cooler]':'',
+'[Headphones]':'', '[Fan]':'', '[Prebuilt]':'', '[Headset]':'', '[Optical Drive]':'', '[OS]':'', '[Speakers]':'',
+'[Keyboard]':'', '[Networking]':'', '[Furniture]':'', '[Mouse]':'', '[Bundle]':'', '[HTPC]':'', '[Cables]':'',
+'[Flash Drive]':'', '[Router]':'', '[Mic]':''}
+
 PriceRegex = re.compile(r'\$\d+\.?\d{2}')
 url = 'https://old.reddit.com/r/buildapcsales/top/'
 headers = {'User-Agent': 'Mozilla/5.0'}
-PartLinks = {'CPU':'','GPU':'', 'RAM':'', 'HDD':'', 'SSD':'', 'Motherboard':'', 'Monitor':'',
-'PSU':'', 'Webcam':'', 'Controller':'', 'Laptop':'', 'Other':'', 'VR':'', 'Case':'', 'Cooler':'',
-'Headphones':'', 'Fan':'', 'Prebuilt':'', 'Headset':'', 'Optical Drive':'', 'OS':'', 'Speakers':'',
-'Keyboard':'', 'Networking':'', 'Furniture':'', 'Mouse':'', 'Bundle':'', 'HTPC':'', 'Cables':'',
-'Flash Drive':'', 'Router':'', 'Mic':''}
 
 print(f'Here are the cheapest PC parts from {url} for today:')
 res = requests.get(url, headers = headers)
@@ -43,7 +44,8 @@ for k,v in PartCategory.items():
     if v == 10000:
         continue
     else:
-        print(k + ': $' + str(v) + ' URL: ' + PartLinks[k])
+        print(k[1:-1] + ': $' + str(v) + ' URL: ' + PartLinks[k])
+        print('\n')
 
-
+input('Enter any key to exit..')
 
