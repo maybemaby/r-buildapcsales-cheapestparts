ed319630b3ed1617e3771f1af3e07c6a3509a637
maybemaby <74226064+maybemaby@users.noreply.github.com>
2020-12-12 16:17:43 -0800

    Finished basic functionality.

--- buildapcsales-cheapestparts.py
+++ buildapcsales-cheapestparts.py
@@ -17,14 +17,20 @@ PartRegex = re.compile(r'(\[.{1,10}\])')
 # Looping through posts for the day.
 print('Here are the cheapest PC parts from r/buildapcsales for today.')
 cheapParts = {}
-partLinks = {}
 for submission in reddit.subreddit('buildapcsales').top('day', limit = 50):
     price = PriceRegex.search(submission.title)
+    price = float(price.group()[1:])
     part = PartRegex.search(submission.title)
     partcategory = part.group(1)
+    postdata = []
     if partcategory not in cheapParts:
-        cheapParts.setdefault(partcategory.upper(), float(price.group()[1:]))
-    elif cheapParts[partcategory.upper()] > float(price.group()[1:]):
-        cheapParts[partcategory.upper()] = float(price.group()[1:])
+        postdata.append(submission.title)
+        postdata.append(price)
+        postdata.append(submission.url)
+        cheapParts.setdefault(partcategory.upper(), postdata)
+    elif cheapParts[partcategory.upper()][1] > price:
+        cheapParts[partcategory.upper()][1] = price
 
-print(cheapParts)
+# Results
+for k,v in cheapParts.items():
+    print(v[0] +'\nURL:' + v[-1])
